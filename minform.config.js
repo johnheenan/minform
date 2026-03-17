57b8d7fee5724701adb2d30361d812841e018676
John Heenan <john@zgus.com>
2026-03-17 23:31:07 +1000

    create minform.config.js and add in pico.css

---
+++ minform.config.js
@@ -0,0 +1,39 @@
+import fs from "fs";
+
+export default function minformConfig(eleventyConfig) {
+  eleventyConfig.addFilter("dataNavigation", function (nav) {
+    const url = this.page.url;
+    return {
+      nav_pages: nav,
+      current_url: url,
+    };
+  });
+
+  eleventyConfig.addFilter("file_included", function (rawInput) {
+    const re = /{%[\s]+include /s; //if include used then use a test to variables that are global
+    return re.test(rawInput);
+  });
+
+  eleventyConfig.addFilter("mailformResponseTarget", function (target) {
+    if (!target || target == "internal") {
+      return "internalResponse";
+    }
+    return target;
+  });
+
+  eleventyConfig.addFilter("addformurl", function (contact, formurl) {
+    let urlpath = formurl.formpath;
+    if (formurl.corsprod == "true" || formurl.prod == "false" || !formurl.prod) urlpath = formurl.corsurl + urlpath;
+    return { ...contact, ...{ urlpath: urlpath } };
+  });
+
+  eleventyConfig.addShortcode("source", function (file) {
+    if (!file) file = this.page.inputPath;
+    let contents = fs.readFileSync(file, "utf8");
+    return contents;
+  });
+
+  eleventyConfig.addShortcode("npm_package_version", function () {
+    return process.env.npm_package_version;
+  });
+}
--- site-1/diff.md
+++ site-1/diff.md
@@ -1,4 +1,5 @@
 ---
+layout: default-pico.njk
 eleventyNavigation:
   key: diff
   title: Diff
